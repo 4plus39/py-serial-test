92ce4d030da137e41d47e968d56db5e004b372d8
Shijia chan <Chan.Shijia@inventec.com>
2019-06-12 17:35:07 +0800

    Dynamic status showing

--- testui.py
+++ testui.py
@@ -8,7 +8,7 @@ running = False
 class UI:
     def __init__(self, master):
         master.title("Serial Port Test  ")
-        master.geometry("200x90")
+        master.geometry("200x120")
 
         frame = tk.Frame(master)
         frame.pack()
@@ -22,16 +22,34 @@ class UI:
         self.device = ttk.Combobox(frame, width=16)
         self.device['values'] = ()
         self.device.config(state='readonly')
-        self.device.grid(row=1, column=0, padx=0, pady=0,columnspan=2)
+        self.device.grid(row=1, column=0, padx=0, pady=0, columnspan=2)
+
+        self.status = tk.Label(frame, text="Status: ")
+        self.status.grid(row=2, column=0, padx=0, pady=10)
+
+        self.n_status = tk.Label(frame, text="")
+        # self.n_status.grid(row=2, column=1, padx=0, pady=10)
 
     def start_test(self):
-        self.testButton.config(text="Stop test", command=self.stop_test)
         global running
         running = True
+
+        self.testButton.config(text="Stop test", command=self.stop_test)
         # print(self.device.current(), self.device.get())
 
+        self.n_status.grid(row=2, column=1, padx=0, pady=10)
+
     def stop_test(self):
-        self.testButton.config(text="Start test", command=self.start_test)
         global running
         running = False
+
+        self.testButton.config(text="Start test", command=self.start_test)
         # print(self.device.current(), self.device.get())
+
+        self.n_status.grid_forget()
+
+    def status_pass(self):
+        self.n_status.config(text="PASS", bg='green')
+
+    def status_fail(self):
+        self.n_status.config(text="FAILED", bg='red')
