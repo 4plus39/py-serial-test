c5cd4be3b6ed55eb0cb729a69e9aaf662c5d9b44
Shijia chan <Chan.Shijia@inventec.com>
2019-06-12 17:34:45 +0800

    Dynamic status showing

--- mainui.py
+++ mainui.py
@@ -2,24 +2,34 @@ import tkinter as tk
 import testio
 import testui
 
+# CONST NAME
+BAU_RATE = 115200
+TIME_OUT = 0.5
+NULL_LIST = []
 
-def scanning():
+
+def loop():
     # Only do this if the Stop button has not been clicked
-    if testui.running:
-        serial.name = app.device.get()
-        serial.config(115200, 0.5)
-        serial.send()
+    if testui.running and app.device.current() != -1:
+        serial_port.name = app.device.get()
+        serial_port.config(BAU_RATE, TIME_OUT)
+        serial_port.send()
 
-    # After 0.1 second, call scanning again (create a recursive loop)
-    root.after(500, scanning)
+        if serial_port.read() == NULL_LIST:
+            app.status_fail()
+        else:
+            app.status_pass()
+
+    # After 0.1 second, call loop() again (create a recursive loop)
+    root.after(TIME_OUT, loop)
 
 
-serial = testio.SerialPort(None)
-serial.scan()
+serial_port = testio.SerialPort(None)
+serial_port.scan()
 root = tk.Tk()
 app = testui.UI(root)
-app.device['values'] = serial.device
+app.device['values'] = serial_port.device
 
 # After 0.1 second, call scanning
-root.after(100, scanning)
+root.after(TIME_OUT, loop)
 root.mainloop()
