4f56b355381651f2df7f7f6ff23e8e81b3c4aaff
Shijia Chan <Chan.Shijia@inventec.com>
2019-07-01 14:27:28 +0800

    fix Windows PermissionError bug

--- mainui.py
+++ mainui.py
@@ -6,8 +6,10 @@ import const
 
 def loop():
     if testui.FLAG and app.device.get() != const.NULL_STR:
-        serial_port.name = app.device.get()
-        serial_port.config(const.BAU_RATE, const.TIME_OUT)
+        # Repeat config same serial port will show "PermissionError" in Windows OS,but linux wouldn't        
+        if serial_port.name == None:
+            serial_port.name = app.device.get()
+            serial_port.config(const.BAU_RATE, const.TIME_OUT)
         serial_port.send()
 
         if serial_port.read() == const.NULL_LIST:
