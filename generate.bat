02091c49e7716c7805bd8f48c0cfe3b1d9892bce
shijia <zhansj39@gmail.com>
2019-12-11 22:01:41 -0800

    add ico

--- generate.bat
+++ generate.bat
@@ -1,2 +1,2 @@
-pyinstaller -F main.py -n sptc
-pyinstaller -F -w mainui.py -n sptu
+pyinstaller -F main.py -n sptc -i ico\rs_232_female-48_44741.ico
+pyinstaller -F -w mainui.py -n sptu -i ico\rs_232_male-48_45611.ico
